ced4320523c7b29f475f59d2275397a70ec9581c
Itay Vagen <118840916+VagenItay@users.noreply.github.com>
2025-04-28 11:32:04 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine #creates connection to the DB
 from sqlalchemy.ext.declarative import declarative_base #all models base
 from sqlalchemy.orm import sessionmaker #manage connections to the DB efficiently
 
-# Replace with your actual database URL (e.g., SQLite, PostgreSQL, etc.)
 SQLALCHEMY_DATABASE_URL = "sqlite:///./myDataBase.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}) #connects the app to the DB with diff threads access
